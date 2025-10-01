9aa363a2f6af60aec0fb3f111af9b9abc0a6238f
Osama Ibne Hasam <ohasam.knysys@gmail.com>
2025-10-01 18:56:32 +0500

    fix error v2

--- src/api/adminApi.ts
+++ src/api/adminApi.ts
@@ -1,11 +1,24 @@
 import axios from "axios";
+import { useAuthStore } from "@/store/useAuthStore";
 
 const adminApi = axios.create({
   baseURL: "https://adminapi.welliemd.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true,
 });
 
-// no auth headers here unless adminapi requires it
+// Request interceptor to add access token
+adminApi.interceptors.request.use(
+  (config) => {
+    const token = useAuthStore.getState().accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
 export default adminApi;
