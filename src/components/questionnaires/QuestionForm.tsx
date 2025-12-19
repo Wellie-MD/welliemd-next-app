2912972f343dde6a48b2008d78b4f8e8480a2666
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2025-12-19 14:37:21 +0500

    feat: Add BMI grouped question type with height/weight templates - Add 'bmi' question type to QuestionForm dropdown - Update GroupedQuestionBuilder with BMI_TEMPLATES for height/weight sub-questions - Enable grouped question builder for BMI type selection

--- src/components/questionnaires/QuestionForm.tsx
+++ src/components/questionnaires/QuestionForm.tsx
@@ -827,7 +827,7 @@ export function QuestionForm({
                 </SelectItem>
                 <SelectItem value="number">Number</SelectItem>
                 <SelectItem value="date">Date</SelectItem>
-                <SelectItem value="height_weight">Height & Weight</SelectItem>
+                <SelectItem value="bmi">BMI (Height, Weight & Auto-Calculate)</SelectItem>
                 <SelectItem value="consent">Consent Checkbox</SelectItem>
                 <SelectItem value="file_upload">File Upload</SelectItem>
                 <SelectItem value="checkout">
@@ -855,7 +855,8 @@ export function QuestionForm({
 
           {/* Grouped Question Builder */}
           {(formData.question_type === "personal_details" ||
-            formData.question_type === "shipping_address") && (
+            formData.question_type === "shipping_address" ||
+            formData.question_type === "bmi") && (
             <GroupedQuestionBuilder
               groupType={formData.question_type}
               subQuestions={subQuestions}
