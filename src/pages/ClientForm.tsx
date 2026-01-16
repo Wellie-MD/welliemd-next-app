c19e4bf734064810a3d2211ef4056f5c687287d8
yasir1brahim <yasir.ibrahim@gmail.com>
2026-01-16 15:49:11 +0500

    enabled the show password field

--- src/pages/ClientForm.tsx
+++ src/pages/ClientForm.tsx
@@ -730,7 +730,6 @@ export default function ClientForm() {
                       size="sm"
                       className="absolute right-0 top-0 h-full px-3 py-2 hover:bg-transparent"
                       onClick={() => setShowPassword(!showPassword)}
-                      disabled={isEditMode}
                     >
                       {showPassword ? (
                         <EyeOff className="h-4 w-4 text-muted-foreground" />
