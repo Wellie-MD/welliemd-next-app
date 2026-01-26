2cbea6c814bb8b1b26035fc943f9d5eec0a201d3
Yasir Ibrahim <yasir.ibrahim@gmail.com>
2026-01-26 16:48:55 +0500

    feat: update follow-up redirection flow to support secure access tokens and enhanced questionnaire pre-filling

---
+++ src/features/treatments/index.ts
@@ -0,0 +1,6 @@
+/**
+ * Treatments feature exports.
+ */
+export { AvailableTreatmentsList } from './AvailableTreatmentsList';
+export { getAvailableTreatments, startNewTreatment } from './api';
+export type { AvailableTreatment, StartTreatmentResponse } from './api';
